f114e10c433e8a0d38363e7a377ad1a2cb4fc058
Maxinne D. Ogale <maxinelat65@gmail.com>
2024-04-29 22:35:42 +0800

    Revert "Merge branch 'main' of https://github.com/Maxyyey/Maintenance"
    This reverts commit c2f7e9f76296a272275cd7af0ba5757170ca4b96, reversing
    changes made to 2afcea38a9992c7c832127c328bb4052a0de0100.

--- src/app/main/components/personnelsetup/personnelsetup.component.ts
+++ src/app/main/components/personnelsetup/personnelsetup.component.ts
@@ -5,14 +5,10 @@ import { AddUserComponent } from './adduser/adduser.component';
 import { EditUserComponent } from './edituser/edituser.component';
 import { ArchivessComponent } from './archivess/archivess.component';
 
-
-
 @Component({
   selector: 'app-personnelsetup',
   templateUrl: './personnelsetup.component.html',
   styleUrl: './personnelsetup.component.scss',
-
-
 })
 export class PersonnelSetupComponent implements OnInit{
   constructor(private dialogRef : MatDialog) { }
