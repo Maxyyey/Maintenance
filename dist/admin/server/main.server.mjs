f18a6f694c5d5a06e63613e78cb817ec3e9b5797
Tamayogerard <90178180+Tamayogerard@users.noreply.github.com>
2024-04-30 01:42:59 +0800

    ...

--- dist/admin/server/main.server.mjs
+++ dist/admin/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-4JS2HHBT.mjs";import"./chunk-SHETQOXP.mjs";import"./chunk-CYTTF7BJ.mjs";import"./chunk-RM7DWBUR.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-JO4AWPFF.mjs";import"./chunk-SHETQOXP.mjs";import"./chunk-CYTTF7BJ.mjs";import"./chunk-RM7DWBUR.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
---
+++ src/app/main/components/cataloging/components/academicproject/chtm/archivechtmpopup1/archivechtmpopup1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-archivechtmpopup1',
+  templateUrl: './archivechtmpopup1.component.html',
+  styleUrl: './archivechtmpopup1.component.scss'
+})
+export class Archivechtmpopup1Component {
+
+}
