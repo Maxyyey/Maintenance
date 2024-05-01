7d2d4444e8ccc223a9ea980a7e047d2c525f31f9
Xskfy <150583210+Xskfy@users.noreply.github.com>
2024-05-01 17:18:55 +0800

    Inventory clear history popup
    -Added popup for clear button in clear history

--- src/app/main/components/inventory/clearhistory/clearhistory.component.ts
+++ src/app/main/components/inventory/clearhistory/clearhistory.component.ts
@@ -1,19 +1,34 @@
-import { Component,Inject } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog'; // Import MatDialog
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Clearhistory1Component } from '../clearhistory1/clearhistory1.component';
 
 @Component({
   selector: 'app-clearhistory',
   templateUrl: './clearhistory.component.html',
-  styleUrl: './clearhistory.component.scss'
+  styleUrls: ['./clearhistory.component.scss'] // Corrected property name
 })
-export class ClearhistoryComponent  {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private ref: MatDialogRef<ClearhistoryComponent>) {
+export class ClearhistoryComponent {
 
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private dialogRef: MatDialogRef<ClearhistoryComponent>, private dialog: MatDialog) {
+    // Inject MatDialogRef and MatDialog
   }
+
   closepopup() {
-    this.ref.close('Closed using function');
+    this.dialogRef.close('Closed using function');
   }
-}
 
+  ngOnInit(): void { }
+
+  //onEditBtnClick(){
+    // Open the EditPopupComponent dialog
+    //const dialogRef = this.dialog.open(EditCahsPopupComponent, {});
+    // If you need to do something with the dialog reference, you can assign it to a class variable
+    // this.dialogRef = dialogRef;
+  
+  onClear1BtnClick(){
+    const dialogRef = this.dialog.open(Clearhistory1Component, {});
+  }
+
+}
 
