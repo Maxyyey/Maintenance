df53727c15031d411e2e20217a9b1d400143e47f
Tamayogerard <90178180+Tamayogerard@users.noreply.github.com>
2024-05-03 02:05:06 +0800

    archive di pa tapos wag gagalawin yung mga archive

--- src/app/main/components/addlocker/components/archives/archives.component.ts
+++ src/app/main/components/addlocker/components/archives/archives.component.ts
@@ -1,19 +1,45 @@
 import { Component, Inject } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialogRef as MyMatDialogRef} from '@angular/material/dialog';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import Swal from 'sweetalert2';
+
+interface MyOption {
+  value: string;
+  label: string;
+}
 @Component({
   selector: 'app-archive',
   templateUrl: './archives.component.html',
   styleUrl: './archives.component.scss',
+  standalone: true,
+  imports: [
+    FormsModule,
+    CommonModule
+  ],
   
 })
 export class ArchiveComponent {
 
-  // Component logic here
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private ref: MatDialogRef<ArchiveComponent>) {
+  constructor(public dialogRef: MyMatDialogRef<ArchiveComponent>) {}
 
-  }
   closepopup() {
-    this.ref.close('Closed using function');
+    this.dialogRef.close('Closed using function');
   }
+
+  archiveBox(){
+    this.dialogRef.close('Closed using function');
+    Swal.fire({
+      title: "Archiving complete!",
+      text: "Project has been safely archived.",
+      icon: "success",
+      confirmButtonText: 'Close',
+      confirmButtonColor: "#777777",
+    });
+  
+}
 }
