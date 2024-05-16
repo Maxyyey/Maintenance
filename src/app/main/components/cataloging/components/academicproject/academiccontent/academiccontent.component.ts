8ca28f9df38b8bf2dd371c23839699fc8ab98e39
Maxinne D. Ogale <maxinelat65@gmail.com>
2024-05-16 22:38:20 +0800

    ..

--- src/app/main/components/cataloging/components/academicproject/academiccontent/academiccontent.component.ts
+++ src/app/main/components/cataloging/components/academicproject/academiccontent/academiccontent.component.ts
@@ -2,15 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { AddiconacadComponent } from './addiconacad/addiconacad.component';
-import { ViewComponent } from './view/view.component';
+import { ViewComponent } from './viewccs/view.component';
+import { ViewCbaComponent } from './cba/viewcba.component';
 
 @Component({
   selector: 'app-academiccontent',
   templateUrl: './academiccontent.component.html',
-  styleUrls: ['./academiccontent.component.scss'],
+  styleUrl: './academiccontent.component.scss',
 })
 export class AcademiccontentComponent implements OnInit {
-  constructor(private dialogRef : MatDialog) { }
+  constructor(private dialogRef: MatDialog) { }
 
   ngOnInit(): void {
     // Initialize component properties or fetch data here
@@ -22,6 +23,10 @@ export class AcademiccontentComponent implements OnInit {
 
   onViewBtnClick(){
     this.dialogRef.open(ViewComponent, {});
+  }
+
+  onViewCbaBtnClick(){
+    this.dialogRef.open(ViewCbaComponent, {});
   }
 
   // Other component logic goes here
