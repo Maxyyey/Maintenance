d6498bd9f155e1771763b05de2ccbe831770fcb1
severusraj <rangelojam@gmail.com>
2024-07-20 22:07:51 +0800

    adding auth interceptor

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { apiUrl } from '@app/config/config';
-import { Observable, Subject, catchError, tap } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
 import { HeaderService } from './header.service';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,23 +13,26 @@ export class AuthService {
 
   constructor(private http: HttpClient, private router: Router, private headers: HeaderService) { }
 
-  login(credentials: { username: string, password:string}) {
-
+  login(credentials: { username: string, password: string }): Observable<any> {
     return this.http.post<any>(`${apiUrl}/login/maintenance`, credentials).pipe(
-      tap((response => {
-        if(response.token){
-          sessionStorage.setItem('token', response.token)
+      tap(response => {
+        if (response.token) {
+          sessionStorage.setItem('token', response.token);
         }
-      }))
-    )
+      })
+    );
   }
 
-  logout() {
-    return this.http.post<any>(`${apiUrl}/logout`, {}, {headers: this.headers.get() } ).pipe(
-      tap((response => {
-          sessionStorage.clear()
-          this.router.navigate(['/login'])
-      }))
-    )
+  logout(): Observable<any> {
+    return this.http.post<any>(`${apiUrl}/logout`, {}, { headers: this.headers.get() }).pipe(
+      tap(response => {
+        sessionStorage.clear();
+        this.router.navigate(['/login']);
+      })
+    );
+  }
+
+  getToken(): string | null {
+    return sessionStorage.getItem('token');
   }
 }
