fd09d0602ceaf52cff045c20eb6d922adf1e246f
Chaws-Jewn <150521807+Chaws-Jewn@users.noreply.github.com>
2025-07-12 11:32:32 +0800

    personnel add and edit
    - removed password to edit
    - added position to both

--- src/app/main/components/personnelsetup/adduser/adduser.component.ts
+++ src/app/main/components/personnelsetup/adduser/adduser.component.ts
@@ -1,56 +1,53 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { FormBuilder } from '@angular/forms';
-import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { Component, Inject, OnInit } from "@angular/core";
+import { MatDialogRef } from "@angular/material/dialog";
+import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { FormBuilder } from "@angular/forms";
+import { Router } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
 
-
-import Swal from 'sweetalert2';
-import { Settings } from 'node:http2';
-import { PersonnelService } from '@app/services/personnel.service';
-import { response } from 'express';
-import { error } from 'node:console';
+import Swal from "sweetalert2";
+import { Settings } from "node:http2";
+import { PersonnelService } from "@app/services/personnel.service";
+import { response } from "express";
+import { error } from "node:console";
 
 interface MyOption {
   value: string;
   label: string;
 }
 @Component({
-  selector: 'app-adduser',
-  templateUrl: './adduser.component.html',
-  styleUrl: './adduser.component.scss',
+  selector: "app-adduser",
+  templateUrl: "./adduser.component.html",
+  styleUrl: "./adduser.component.scss",
   standalone: true,
-  imports: [
-    FormsModule,
-    CommonModule
-  ],
-  
+  imports: [FormsModule, CommonModule],
 })
 export class AddUserComponent {
-
   form: {
-    first_name: string,
-    middle_name: string,
-    last_name: string,
-    ext_name: string,
-    username: string,  //this is email
-    password: string,
-    role: string
-  }
+    first_name: string;
+    middle_name: string;
+    last_name: string;
+    ext_name: string;
+    username: string; //this is email
+    password: string;
+    position: string;
+    role: string;
+  };
   constructor(
     private ref: MatDialogRef<AddUserComponent>,
-    private personnelService: PersonnelService) {
-      this.form = {
-        first_name: '',
-        middle_name: '',
-        last_name: '',
-        ext_name: '',
-        username: '',  //this is email
-        password: '',
-        role: ''
-      }
+    private personnelService: PersonnelService
+  ) {
+    this.form = {
+      first_name: "",
+      middle_name: "",
+      last_name: "",
+      ext_name: "",
+      username: "", //this is email
+      password: "",
+      position: "",
+      role: "",
+    };
   }
 
   closepopup() {
@@ -63,34 +60,34 @@ export class AddUserComponent {
       middle_name: this.form.middle_name,
       last_name: this.form.last_name,
       ext_name: this.form.ext_name,
-      username: this.form.username,  
+      username: this.form.username,
       password: this.form.password,
-      role: JSON.stringify([this.form.role])
-    }
+      position: this.form.position,
+      role: JSON.stringify([this.form.role]),
+    };
 
-    console.log(this.form)
-    if(this.form.username.includes('@')) {
+    console.log(this.form);
+    if (this.form.username.includes("@")) {
       this.personnelService.createPersonnel(form).subscribe(
-        response => {
+        (response) => {
           this.ref.close(response);
           Swal.fire({
             title: "Add successful!",
             text: "The changes have been saved.",
             icon: "success",
-            confirmButtonText: 'Close',
+            confirmButtonText: "Close",
             confirmButtonColor: "#777777",
           });
         },
-        error => {
-          console.error(error)
-          if(error.status === 422) {
+        (error) => {
+          console.error(error);
+          if (error.status === 422) {
             Swal.fire({
               title: "error!",
               text: "Invalid input.",
               icon: "error",
             });
-          } 
-          else {
+          } else {
             Swal.fire({
               title: "error!",
               text: "Something went wrong, please try again later.",
@@ -98,50 +95,49 @@ export class AddUserComponent {
             });
           }
         }
-      )
+      );
     } else {
       Swal.fire({
         title: "Invalid Email Address!",
         text: "Personnel not saved.",
         icon: "error",
-        confirmButtonText: 'Close',
+        confirmButtonText: "Close",
         confirmButtonColor: "#777777",
       });
     }
-    
   }
 
   // SWEETALERT UPDATE POPUP
-  addBox(){
+  addBox() {
     Swal.fire({
       title: "Add User",
       text: "Are you sure you want to add this user?",
       icon: "warning",
       showCancelButton: true,
-      confirmButtonText: 'Yes',
-      cancelButtonText: 'Cancel',
+      confirmButtonText: "Yes",
+      cancelButtonText: "Cancel",
       confirmButtonColor: "#4f6f52",
       cancelButtonColor: "#777777",
     }).then((result) => {
       if (result.isConfirmed) {
-        this.addUser()
+        this.addUser();
       }
     });
   }
 
-  cancelBox(){
+  cancelBox() {
     Swal.fire({
       title: "Are you sure you want to cancel adding personnel?",
       text: "Your changes will not be saved.",
       icon: "question",
       showCancelButton: true,
-      confirmButtonText: 'Yes',
-      cancelButtonText: 'No',
+      confirmButtonText: "Yes",
+      cancelButtonText: "No",
       confirmButtonColor: "#AB0E0E",
       cancelButtonColor: "#777777",
     }).then((result) => {
       if (result.isConfirmed) {
-        this.closepopup()
+        this.closepopup();
         const Toast = Swal.mixin({
           toast: true,
           position: "top-end",
@@ -151,15 +147,13 @@ export class AddUserComponent {
           didOpen: (toast) => {
             toast.onmouseenter = Swal.stopTimer;
             toast.onmouseleave = Swal.resumeTimer;
-          }
+          },
         });
         Toast.fire({
           icon: "error",
-          title: "Changes not saved."
+          title: "Changes not saved.",
         });
       }
     });
   }
 }
-
-
